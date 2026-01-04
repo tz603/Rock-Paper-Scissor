a2c39096624956647593d6de464a941712806247
tz603 <traviszheng408@gmail.com>
2026-01-04 05:00:06 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,23 +1,39 @@
 import random
 
-comChoice = ""
+while True:
+  bot_choice = ""
+  bot_choice = random.randint(1, 3)
+  if bot_choice == 1:
+      bot_choice = "ROCK"
+  elif bot_choice == 2:
+      bot_choice = "PAPER"
+  else:
+      bot_choice = "SCISSOR"
 
-comGuess = random.randint(1, 3)
-if comGuess == 1:
-    comChoice = "ROCK"
-elif comGuess == 2:
-    comChoice = "PAPER"
-else:
-    comChoice = "Scissor"
+  player_guess = input("Type in your play (Rock, Paper, or Scissor): ")
+  player_choice = player_guess.upper()
 
-print(comChoice)
+  if player_choice == bot_choice:
+    print("It's a tie!")
 
-playerGuess = input("Type in your play: (Rock, Paper, or Scissor)")
-playerChoice = playerGuess.capitalize
+  if player_choice == "ROCK":
+      if bot_choice == "PAPER":
+          print("You lose!", bot_choice, "covers", player_choice)
+      else:
+          print("You win!", player_choice, "smashes", bot_choice)
 
-
-# def game(comChoice, playerChoice):
-#     return result
-
-# game(comGuess, guess)
-
+  if player_choice == "PAPER":
+      if bot_choice == "SCISSOR":
+          print("You lose!", bot_choice, "cuts", player_choice)
+      else:
+          print("You win!", player_choice, "covers", bot_choice)
+    
+  if player_choice == "SCISSOR":
+      if bot_choice == "ROCK":
+          print("You lose!", bot_choice, "smashes", player_choice)
+      else:
+          print("You win!", player_choice, "cuts", bot_choice)
+  repeat_entered = input("Would you like to play again? (Yes/No): ")
+  repeat_choice = repeat_entered.upper()
+  if repeat_choice == 'NO':
+    break
